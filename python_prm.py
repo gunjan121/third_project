18ad86178773f9d395424d272fc46c89c4f0453a
gunjan121 <gunjanplrr@gmail.com>
2024-01-03 17:40:52 +0530

    multiplication add

--- python_prm.py
+++ python_prm.py
@@ -3,5 +3,5 @@ print("Hello git Hub")
 print(12+32)
 print("just try to update program from local to remote ")
 print("substraction of two number is ",232-23)
-
+print("multiplicaiton of two number is ",23*2)
 print("Thank You!")
