01e06fa211c934b5958d73cc60a7eda1a5bc4fa3
gunjan121 <gunjanplrr@gmail.com>
2024-01-04 20:54:48 +0530

    added some funtion

--- python_prm.py
+++ python_prm.py
@@ -6,3 +6,31 @@ print("substraction of two number is ",232-23)
 print("multiplicaiton of two number is ",23*2)
 print("Division of two number is ",500/5)
 print("Thank You!")
+
+
+x=2
+y=2.0
+print("x is ",x)
+print("y is ",y)
+if(x==y):
+    print("x and y are equall")
+else:
+    print("both are diffenter")
+
+def fun():
+    x=100
+    return x
+x=+1
+v=fun()
+print(v)
+
+
+file=open("C:\Users\Admin\Desktop\mytext.txt")
+file.read()
+file.close()
+
+
+
+
+
+
